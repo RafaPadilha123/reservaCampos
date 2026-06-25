6fbdb97bc03b23b67f2f9f0ddf6efa87b4c40c2e
Rafael Padilha <rafaelpadilha.2020@alunos.utfpr.edu.br>
2026-06-25 15:02:02 -0300

    feat(backend): aplica blindagem DTO, padronização de tráfego e esteira TDD funcional (ID6, ID9, ID10, ID11, ID12)

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,12 +1,14 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'; // 🌟 Importa o Swagger
+import { AppModule } from './app.module';
+import { TransformInterceptor } from './transform.interceptor';
+import { AllExceptionsFilter } from './http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors();
-
+  // ID6: Validação global
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -15,6 +17,22 @@ async function bootstrap() {
     }),
   );
 
+  // ID9: Padronização de respostas e erros
+  app.useGlobalInterceptors(new TransformInterceptor());
+  app.useGlobalFilters(new AllExceptionsFilter());
+
+  // 🌟 ID12: CONFIGURAÇÃO DO SWAGGER
+  const config = new DocumentBuilder()
+    .setTitle('🏟️ Arena Campos API')
+    .setDescription('Documentação interativa do sistema de reserva de quadras esportivas')
+    .setVersion('1.0')
+    .addBearerAuth() // Adiciona o campo de colocar o Token JWT lá no topo do Swagger
+    .build();
+    
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document); // Define que a rota será http://localhost:3000/api
+
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
