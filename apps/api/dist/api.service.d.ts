ba9462dc70247fa2a0a47236065a3101976de021
Rafael Padilha <rafaelpadilha.2020@alunos.utfpr.edu.br>
2026-06-26 08:46:28 -0300

    fix: remove ghost api service file from backend

--- apps/api/dist/api.service.d.ts
+++
@@ -1,7 +0,0 @@
-import { HttpService } from '@nestjs/axios';
-export declare class ApiService {
-    private readonly httpService;
-    private readonly baseUrl;
-    constructor(httpService: HttpService);
-    request(method: 'get' | 'post' | 'patch' | 'delete', endpoint: string, data?: any, token?: string): Promise<any>;
-}
